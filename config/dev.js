5967c3287830cbc84a4ef62f51f6df8737baf756
RenanMomesso <renan_momesso@hotmail.com>
2020-06-09 12:32:40 -0300

    update

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:"mongodb+srv://sormer:Sormerrenan123@cluster0-hkwdl.mongodb.net/boilerplate?retryWrites=true&w=majority"
+}
--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan')
+const {User} = require('./model/user')
+const env = require('dotenv').config()
+const config = require('./config/key')
 
 
-mongoose.connect("mongodb+srv://sormer:Sormerrenan123@cluster0-hkwdl.mongodb.net/boilerplate?retryWrites=true&w=majority",{useNewUrlParser:true, useUnifiedTopology:true}, console.log('connected'))
+//db connection
+mongoose.connect(config.mongoURI,{useNewUrlParser:true, useUnifiedTopology:true}, console.log('connected'))
 
-app.get('/',(req,res)=>{
-    res.json({ok:true})
+//midlewares
+app.use(express.json({limit:'50mb'}))
+app.use(express.urlencoded({extended:true}))
+app.use(cookieParser())
+app.use(morgan('dev'))
+
+
+
+
+app.post('/',(req,res)=>{
+    let user = new User(req.body)
+    user.save((err,user)=>{
+        if(err) return res.json({success:false, err}) 
+        return res.status(200).json({user})
+    })
 })
 
 app.listen(3000, console.log('funcionando'))
